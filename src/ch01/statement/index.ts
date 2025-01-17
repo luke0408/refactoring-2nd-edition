bd51656aacf88e2cf40dc09f430364bf4ab923ba
zjvl324 <zjvl324@gmail.com>
2025-01-17 14:02:44 +0000

    refactor: amountFor 매서드 분리

--- src/ch01/statement/index.ts
+++ src/ch01/statement/index.ts
@@ -15,23 +15,7 @@ export const statement = (invoice: InvoiceType.Invoice, plays: PlayType.Plays) =
     const play = plays[perf.playID];
     let thisAmount = 0;
 
-    switch (play?.type) {
-      case 'tragedy':
-        thisAmount = 40000;
-        if (perf.audience > 30) {
-          thisAmount += 1000 * (perf.audience - 30);
-        }
-        break;
-      case 'comedy':
-        thisAmount = 30000;
-        if (perf.audience > 20) {
-          thisAmount += 10000 + 500 * (perf.audience - 20);
-        }
-        thisAmount += 300 * perf.audience;
-        break;
-      default:
-        throw new Error(`알 수 없는 장르: ${play?.type}`);
-    }
+    thisAmount = amountFor(perf, play);
 
     // 포인트를 제공한다.
     volumeCredits += Math.max(perf.audience - 30, 0);
@@ -48,4 +32,26 @@ export const statement = (invoice: InvoiceType.Invoice, plays: PlayType.Plays) =
   result += `적립 포인트: ${volumeCredits}점\n`;
 
   return result;
+};
+
+const amountFor = (performance: InvoiceType.PerformanceInfo, play: PlayType.PlayInfo): number => {
+  let thisAmount: number = 0;
+  switch (play?.type) {
+    case 'tragedy':
+      thisAmount = 40000;
+      if (performance.audience > 30) {
+        thisAmount += 1000 * (performance.audience - 30);
+      }
+      break;
+    case 'comedy':
+      thisAmount = 30000;
+      if (performance.audience > 20) {
+        thisAmount += 10000 + 500 * (performance.audience - 20);
+      }
+      thisAmount += 300 * performance.audience;
+      break;
+    default:
+      throw new Error(`알 수 없는 장르: ${play?.type}`);
+  }
+  return thisAmount;
 };
