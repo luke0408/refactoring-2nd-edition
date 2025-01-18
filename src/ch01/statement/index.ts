ae7d6e7726ff89c86f5beb4dbb2fdee25541e0c8
zjvl324 <zjvl324@gmail.com>
2025-01-18 06:13:28 +0000

    refactor: 파일 구조 분리

--- src/ch01/statement/index.ts
+++ src/ch01/statement/index.ts
@@ -1,4 +1,5 @@
 import { InvoiceType, PlayType, StatementType } from '../types';
+import { createStatementData } from './createStatementData';
 
 /**
  * 연극에 대한 청구 내역과 총액, 적립 포인트를 반환한다.
@@ -42,111 +43,5 @@ function renderPlainText(data: StatementType.StatementData) {
       currency: 'USD',
       minimumFractionDigits: 2,
     }).format(number / 100);
-  }
-}
-
-/**
- * statement에 필요한 데이터를 처리한다.
- * 
- * @param invoice 
- * @param plays 
- * @returns 
- */
-function createStatementData(invoice: InvoiceType.Invoice, plays: PlayType.Plays) {
-  const statementData: StatementType.StatementData = {} as StatementType.StatementData;
-  statementData.customer = invoice.customer;
-  statementData.performances = invoice.performances.map(enrichPerformance);
-  statementData.totalAmount = totalAmount(statementData);
-  statementData.totalVolumeCredits = totalVolumeCredits(statementData);
-  return statementData;
-
-  /**
-   * 공연 정보를 추가한다.
-   *
-   * @param performance
-   * @returns
-   */
-  function enrichPerformance(performance: InvoiceType.PerformanceInfo): StatementType.PerformanceInfo {
-    const result = Object.assign({}, performance) as StatementType.PerformanceInfo;
-    result.play = playFor(result);
-    result.amount = amountFor(result);
-    result.volumeCredits = volumeCreditsFor(result);
-
-    return result;
-  }
-
-  /**
-   * performance를 통해 play 값을 구한다.
-   *
-   * @param performance
-   * @returns
-   */
-  function playFor(performance: InvoiceType.PerformanceInfo): PlayType.PlayInfo {
-    return plays[performance.playID];
-  }
-
-  /**
-   * 청구 내역에 대한 금액을 구한다.
-   *
-   * @param performance
-   * @returns
-   */
-  function amountFor(performance: StatementType.PerformanceInfo): number {
-    let thisAmount: number = 0;
-    switch (performance.play.type) {
-      case 'tragedy':
-        thisAmount = 40000;
-        if (performance.audience > 30) {
-          thisAmount += 1000 * (performance.audience - 30);
-        }
-        break;
-      case 'comedy':
-        thisAmount = 30000;
-        if (performance.audience > 20) {
-          thisAmount += 10000 + 500 * (performance.audience - 20);
-        }
-        thisAmount += 300 * performance.audience;
-        break;
-      default:
-        throw new Error(`알 수 없는 장르: ${performance.play.type}`);
-    }
-    return thisAmount;
-  }
-
-  /**
-   * totalAmount를 구한다.
-   *
-   * @returns
-   */
-  function totalAmount(data: StatementType.StatementData) {
-    return data.performances
-      .reduce((total, p) => total + p.amount, 0);
-  }
-
-  /**
-   * volumeCredites를 구한다.
-   *
-   * @returns
-   */
-  function totalVolumeCredits(data: StatementType.StatementData) {
-    return data.performances
-      .reduce((total, p) => total + p.volumeCredits, 0)
-  }
-
-  /**
-   * 적립 포인트를 계산한다.
-   *
-   * @param performance
-   * @returns
-   */
-  function volumeCreditsFor(performance: StatementType.PerformanceInfo): number {
-    let volumeCredits = 0;
-    volumeCredits += Math.max(performance.audience - 30, 0);
-
-    if ('comedy' === performance.play.type) {
-      volumeCredits += Math.floor(performance.audience / 5);
-    }
-
-    return volumeCredits;
   }
 }
