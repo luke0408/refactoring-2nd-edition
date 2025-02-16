c31e17ff7806bdaaa287f10b77ceb032c6d15734
zjvl324 <zjvl324@gmail.com>
2025-02-16 03:56:08 +0000

    style: format

--- test/ch04/buyer.spec.ts
+++ test/ch04/buyer.spec.ts
@@ -1,17 +1,19 @@
 import { Buyer } from '../../src/ch04/Actor';
 
 describe('Buyer', () => {
-  it('생성 시 구매자의 정보는 올바르게 설정되어야 한다.', () => {
-    //given
-    const nickname = 'buyer123';
-    const balance = 2000000;
+  describe('생성 시', () => {
+    it('구매자의 정보는 올바르게 설정되어야 한다.', () => {
+      //given
+      const nickname = 'buyer123';
+      const balance = 2000000;
 
-    // when
-    const buyer = new Buyer(nickname, balance);
+      // when
+      const buyer = new Buyer(nickname, balance);
 
-    // then
-    expect(buyer.nickname).toBe(nickname);
-    expect(buyer.balance).toBe(balance);
-    expect(buyer.products).toHaveLength(0);
+      // then
+      expect(buyer.nickname).toBe(nickname);
+      expect(buyer.balance).toBe(balance);
+      expect(buyer.products).toHaveLength(0);
+    });
   });
 });
--- test/ch04/seller.spec.ts
+++ test/ch04/seller.spec.ts
@@ -1,16 +1,15 @@
 import { Seller } from '../../src/ch04/Actor';
 
 describe('Seller', () => {
-  
   describe('생성 시', () => {
     it('판매자의 정보는 올바르게 설정되어야 한다.', () => {
       // given
       const nickname = 'seller123';
       const balance = 0;
-  
+
       // when
       const seller = new Seller('seller123', 0);
-  
+
       // then
       expect(seller.nickname).toBe(nickname);
       expect(seller.balance).toBe(balance);
@@ -18,7 +17,7 @@ describe('Seller', () => {
     });
   });
 
-  describe('상품 등록 시', () => { 
+  describe('상품 등록 시', () => {
     it('필수 정보가 모두 입력되어야 한다.', () => {
       // given
       const nickname = 'seller123';
@@ -26,15 +25,14 @@ describe('Seller', () => {
       const name = '노트북';
       const price = 1000000;
       const quantity = 5;
-  
+
       // when
       const seller = new Seller(nickname, balance);
       seller.registerProduct(name, price, quantity);
-  
+
       // then
       expect(seller.nickname).toBe(nickname);
       expect(seller.balance).toBe(balance);
     });
   });
-
 });
