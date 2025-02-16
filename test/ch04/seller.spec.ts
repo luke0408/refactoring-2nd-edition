9ed500bdb933a258346f0ff42ef880b478a7bb90
zjvl324 <zjvl324@gmail.com>
2025-02-16 03:39:25 +0000

    test: seller test 수정

--- test/ch04/seller.spec.ts
+++ test/ch04/seller.spec.ts
@@ -1,17 +1,21 @@
 import { Seller } from '../../src/ch04/Actor';
 
 describe('Seller', () => {
-  it('생성 시 판매자의 정보는 올바르게 설정되어야 한다.', () => {
-    // given
-    const nickname = 'seller123';
-    const balance = 0;
-
-    // when
-    const seller = new Seller('seller123', 0);
-
-    // then
-    expect(seller.nickname).toBe(nickname);
-    expect(seller.balance).toBe(balance);
-    expect(seller.products).toHaveLength(0);
+  
+  describe('생성 시', () => {
+    it('판매자의 정보는 올바르게 설정되어야 한다.', () => {
+      // given
+      const nickname = 'seller123';
+      const balance = 0;
+  
+      // when
+      const seller = new Seller('seller123', 0);
+  
+      // then
+      expect(seller.nickname).toBe(nickname);
+      expect(seller.balance).toBe(balance);
+      expect(seller.products).toHaveLength(0);
+    });
   });
+  
 });
