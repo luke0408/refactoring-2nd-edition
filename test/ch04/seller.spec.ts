619c65fdf79f67ea34224daf9658726e9e7893f2
zjvl324 <zjvl324@gmail.com>
2025-02-16 03:48:27 +0000

    test: 상품 등록 test 추가

--- test/ch04/seller.spec.ts
+++ test/ch04/seller.spec.ts
@@ -17,5 +17,24 @@ describe('Seller', () => {
       expect(seller.products).toHaveLength(0);
     });
   });
+
+  describe('상품 등록 시', () => { 
+    it('필수 정보가 모두 입력되어야 한다.', () => {
+      // given
+      const nickname = 'seller123';
+      const balance = 0;
+      const name = '노트북';
+      const price = 1000000;
+      const quantity = 5;
   
+      // when
+      const seller = new Seller(nickname, balance);
+      seller.registerProduct(name, price, quantity);
+  
+      // then
+      expect(seller.nickname).toBe(nickname);
+      expect(seller.balance).toBe(balance);
+    });
+  });
+
 });
